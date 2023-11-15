921b20f8922ee8213bd50dc696a1f3de3e7573f6
Moumita Kundu <imoumik@gmail.com>
2023-11-15 19:52:56 +0530

    Adding Reselect Library

--- src/store/categories/categoriesSelector.tsx
+++ src/store/categories/categoriesSelector.tsx
@@ -1,8 +1,16 @@
-export const selectCategoriesMap = (state: any) => {
-    const categoriesMap = state.categories.
-        categories.reduce((acc: { [x: string]: Record<string, any>; }, { title, items }: any) => {
-            acc[title.toLowerCase()] = items;
-            return acc;
-        }, {})
-    return categoriesMap;
-}
+import { createSelector } from 'reselect';
+
+//Reselect Memoization - Input Selector
+const selectCategoryReducer = (state: any) => (state.categories); //Get the Categories Slice
+//Reselect Memoization - Output Selector
+export const selectCategories = createSelector(
+    [selectCategoryReducer], // Attrib1: Array of Input Selectors
+    (categoriesSlice: any) => (categoriesSlice.categories) // Attrib2: Output Selector
+);
+
+export const selectCategoriesMap = createSelector([selectCategories], (categories: any) =>
+    categories.reduce((acc: { [x: string]: any; }, { title, items }: any) => {
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {})
+)
